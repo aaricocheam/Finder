848076356b9b4142c3c5cdd327e030bc14032b85
Anthony <anthony.dam91@gmail.com>
2021-07-14 15:30:12 -0500

    first commit

---
+++ app/src/main/java/app/finder/BaseActivity.java
@@ -0,0 +1,13 @@
+package app.finder;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
